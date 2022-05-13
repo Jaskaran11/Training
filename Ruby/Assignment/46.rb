8b1410c41e314ff722443f175fcfb544ec4f51c5
Jaskaran Singh <jksingh112021@gmail.com>
2022-05-13 16:21:42 +0530

    code to see whether a date is valid or not

---
+++ Ruby/Assignment/46.rb
@@ -0,0 +1,21 @@
+# Given the parameter day, month and year, return whether date is a valid date.
+
+require 'date'
+
+def valid_date(day, month, year)
+  Date.strptime("#{day} #{month} #{year}", '%d %m %Y')
+  rescue Exception => e
+    puts e.message
+    false
+    else
+      puts 'Correct Date'
+end
+
+puts 'Enter the date to see if its a valid date\n Day :'
+day = gets
+puts ' Month : '
+month = gets
+puts ' Year : '
+year = gets
+
+valid_date(day, month, year)
