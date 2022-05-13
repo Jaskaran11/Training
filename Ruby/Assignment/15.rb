c23ef8e0d80787212eb8c5da51b1664dbfb52704
Jaskaran Singh <jksingh112021@gmail.com>
2022-05-13 19:57:16 +0530

    a function that returns only prime number

--- Ruby/Assignment/15.rb
+++ Ruby/Assignment/15.rb
@@ -1,21 +1,22 @@
 # Create a function that takes an array of numbers and returns a new array containing only prime numbers.
-def is_prime(num)
+
+def prime?(num)
   x = 0
   (2...num).each { |i|
     if (num % i).zero?
       x += 1
-      end
-    }
-    return unless x.zero?
-
-    num
     end
+  }
+  return unless x.zero?
 
-  def only_prime(array)
-    prime = []
-    array.each { |a| prime << is_prime(a) }
-    prime.compact
-  end
-  
-    array = [1, 2, 3, 4, 5, 6, 7, 20, 25, 40, 45, 48, 60, 90, 93, 97]
-    print only_prime(array) , "\n" 
+  num
+end
+
+def prime_no(arr)
+  prime = []
+  arr.each { |a| prime << prime?(a) }
+  prime.compact
+end
+
+arr = [2, 4, 7, 9, 11, 13, 71, 97, 120, 159]
+print prime_no(arr), "\n"
