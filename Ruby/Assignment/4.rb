4d39885ace50802a8eca768ed9a2af9ea714d168
Jaskaran Singh <jksingh112021@gmail.com>
2022-05-13 15:49:39 +0530

    code to see whether a number is divisble by 7

--- Ruby/Assignment/4.rb
+++ Ruby/Assignment/4.rb
@@ -1,4 +1,13 @@
 # Given a n-digit large number in form of string check whether divisible by 7 or not.
 
+def divisible_by_seven?(num)
+  remainder = num % 7
+  if remainder.zero?
+    return true
+  else
+    return false
+  end
+end
 
-
+puts "Is 28 Divisible by 7 ? #{divisible_by_seven?(28)}"
+puts "Is 90 Divisible by 7 ? #{divisible_by_seven?(90)}"
