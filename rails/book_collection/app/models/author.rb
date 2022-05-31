22387319b8f969f766d9a059da65e6eea416ab12
Jaskaran Singh <jksingh112021@gmail.com>
2022-05-31 22:23:44 +0530

    strict validations

--- rails/book_collection/app/models/author.rb
+++ rails/book_collection/app/models/author.rb
@@ -2,13 +2,6 @@ class Author < ApplicationRecord
   validates :country, exclusion: { in: %w(www us ca jp),
     message: "%{value} is reserved." }
   validates :email_address, format: { with: /\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})/ }
-  validates :name,
-  uniqueness: {
-
-    message: ->(object, data) do
-      "Hey #{object.name}, #{data[:value]} is already taken"
-    end
-  }
   validates :email_address, uniqueness: true, on: :create
-  validates :dob, numericality: true, on: :account_setup
+  validates :name, presence: { strict: true }
 end
