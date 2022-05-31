7804f7260574977dc973f46af8a2820edd9891ed
Jaskaran Singh <jksingh112021@gmail.com>
2022-05-31 19:16:19 +0530

    used common validation option like allow_nil,allow_blank,message,on

--- rails/book_collection/app/models/author.rb
+++ rails/book_collection/app/models/author.rb
@@ -2,5 +2,13 @@ class Author < ApplicationRecord
   validates :country, exclusion: { in: %w(www us ca jp),
     message: "%{value} is reserved." }
   validates :email_address, format: { with: /\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})/ }
-  validates :name, inclusion: {in: ["Mr", "Mrs"]}
+  validates :name,
+  uniqueness: {
+
+    message: ->(object, data) do
+      "Hey #{object.name}, #{data[:value]} is already taken"
+    end
+  }
+  validates :email_address, uniqueness: true, on: :create
+  validates :dob, numericality: true, on: :account_setup
 end
--- rails/hms/app/models/customer.rb
+++ rails/hms/app/models/customer.rb
@@ -1,13 +1,15 @@
 class Customer < ApplicationRecord
-  validates :name, presence: true
+  validates :name, presence: { message: "must be given please" }
   validates :available, acceptance: { accept: 1, message: "Accept this"}
   validates :address, confirmation: true
   validates :address_confirmation, presence: true
   validates :check_in, comparison: { other_than: :check_out }
   validates :name, length: { minimum: 2 }
-  validates :address, length: { maximum: 10 }
+  validates :address, length: { maximum: 10 }, numericality: { message: "%{value} seems wrong" }
   validates :mobile, length: { is: 10 }
-  validates :gender, absence: true
+  validates :gender, inclusion: { in: %w(male female cantsay),
+    message: "%{value} is not  valid" }, allow_nil: true
   validates :name, uniqueness: true
-  validates :check_in , uniqueness: {scope: :check_out, case_sensitive: false}
+  validates :check_in, uniqueness: { scope: :check_out, case_sensitive: false }
+  validates :check_out, length: { is: 10 }, allow_blank: true
 end
