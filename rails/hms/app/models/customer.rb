339aa403159aa9513dbd8a55932c42e579ab7b8b
Jaskaran Singh <jksingh112021@gmail.com>
2022-05-31 13:57:33 +0530

    length validation in rails

--- rails/hms/app/models/customer.rb
+++ rails/hms/app/models/customer.rb
@@ -3,5 +3,8 @@ class Customer < ApplicationRecord
   validates :available, acceptance: { accept: 1, message: "Accept this"}
   validates :address, confirmation: true
   validates :address_confirmation, presence: true
-  validates :check_out, comparison: {greater_than: :check_in}
+  validates :check_out, comparison: { greater_than: :check_in}
+  validates :name, length: { minimum: 2 }
+  validates :address, length: { maximum: 10}
+  
 end
