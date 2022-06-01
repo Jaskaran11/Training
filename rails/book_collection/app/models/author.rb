822dcdc13d57ead42f2b6126ccb75e90bf258f6b
Jaskaran Singh <jksingh112021@gmail.com>
2022-06-01 14:05:35 +0530

    working with validation errors in rails

--- rails/book_collection/app/models/author.rb
+++ rails/book_collection/app/models/author.rb
@@ -1,7 +1,10 @@
 class Author < ApplicationRecord
   validates :country, exclusion: { in: %w(www us ca jp),
     message: "%{value} is reserved." }
-  validates :email_address, format: { with: /\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})/ }
-  validates :email_address, uniqueness: true, on: :create
-  validates :name, presence: { strict: true }
+  #validates :email_address, format: { with: /\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})/ }
+  #validates :email_address, uniqueness: true, on: :create
+  validates :name, presence: true, length: { minimum: 3 }
+  #validate do |author|
+    #errors.add :base, :invalid, message: "This person is invalid because ..."
+  #end
 end
