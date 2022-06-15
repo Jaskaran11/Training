ab5688d7dffbab8717a7d3514558f824db352ae4
Jaskaran Singh <jksingh112021@gmail.com>
2022-06-15 15:35:17 +0530

    used after_validation callback

--- rails/hosptial_management_system/app/models/appointment.rb
+++ rails/hosptial_management_system/app/models/appointment.rb
@@ -2,9 +2,9 @@ class Appointment < ApplicationRecord
   belongs_to :physician
   #belongs_to :patient
 
-  enum status: {
-    pending: 0,
-    active: 1,
-    archived: 2
-  }
+  validates :date, presence: true
+  after_validation :set_status
+  def set_status
+    self.status = errors.empty?
+  end
 end
