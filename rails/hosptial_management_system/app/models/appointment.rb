65238be10729f93a05df4cfdcf9b00d46525e3cc
Jaskaran Singh <jksingh112021@gmail.com>
2022-06-15 15:54:13 +0530

    before_save callback

--- rails/hosptial_management_system/app/models/appointment.rb
+++ rails/hosptial_management_system/app/models/appointment.rb
@@ -3,7 +3,11 @@ class Appointment < ApplicationRecord
   #belongs_to :patient
 
   validates :date, presence: true
-  after_validation :set_status
+  # after_validation :set_status
+  #def set_status
+    #self.status = errors.empty?
+  # end
+  before_save :set_status
   def set_status
     self.status = errors.empty?
   end
