10c01376ddc328e2c870e5bfcea1928652a0d04a
Jaskaran Singh <jksingh112021@gmail.com>
2022-06-15 18:39:17 +0530

    before_Destroy callback

--- rails/hosptial_management_system/app/models/appointment.rb
+++ rails/hosptial_management_system/app/models/appointment.rb
@@ -7,8 +7,12 @@ class Appointment < ApplicationRecord
   #def set_status
     #self.status = errors.empty?
   # end
-  after_save :set_status
-  def set_status
-    self.status = errors.empty?
+  #after_save :set_status
+  #def set_status
+    #self.status = errors.empty?
+  #end
+  before_destroy :check_id
+  def check_id
+    self.physician_id = errors.empty?
   end
 end
