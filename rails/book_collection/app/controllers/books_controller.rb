df4b907da242f5c49a40ed88d1b8a0a757a75a73
Jaskaran Singh <jksingh112021@gmail.com>
2022-06-23 22:44:41 +0530

    adding update actions:edit/update on the website

--- rails/book_collection/app/controllers/books_controller.rb
+++ rails/book_collection/app/controllers/books_controller.rb
@@ -12,12 +12,29 @@ class BooksController < ApplicationController
   end
 
   def create
+    #Instantiate a new object using form parameters
+    @book = Book.new(book_params)
+    #Save the object
+    if @book.save
+    #If save fails, redirect to the index action
+      redirect_to(books_path)
+    else 
+      # If save fails, redisplay the form so user can fix problem
+      render('new')
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update 
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to(book_path(@book))
+    else 
+      render('edit')
+    end
   end
 
   def delete
@@ -25,4 +42,11 @@ class BooksController < ApplicationController
 
   def destroy
   end
+
+  private 
+
+  def book_params
+    params.require(:book).permit(:title, :price, :rating)
+  end
 end
+
