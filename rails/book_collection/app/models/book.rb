5232268fcdaaf1381e081f910f30f7b7cde24623
Jaskaran Singh <jksingh112021@gmail.com>
2022-06-24 14:56:10 +0530

    using partial template and error messages

--- rails/book_collection/app/models/book.rb
+++ rails/book_collection/app/models/book.rb
@@ -3,10 +3,10 @@ class Book < ApplicationRecord
   # validates :title, absence: true
   #validates :title, length: { maximum: 100,
     #too_long: "%{count} characters is the maximum allowed"}
-  #validates :title, confirmation: true, unless: ->{ title.blank? }
-  #validates :title_confirmation, presence: true, if: Proc.new {|book| !book.title.blank? }
-  #validates :total_page, presence: true, unless: :total_page?
-  #def total_page?
+  # validates :title, confirmation: true, unless: ->{ title.blank? }
+  # validates :title_confirmation, presence: true, if: Proc.new {|book| !book.title.blank? }
+  # validates :total_page, presence: true, unless: :total_page?
+  # def total_page?
     #title.blank?
   #end
   #before_validation :remove_whitespaces
