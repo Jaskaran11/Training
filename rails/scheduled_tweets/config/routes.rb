1c62945f75373e38d07d6cd6dd9035c6c840ca95
Jaskaran Singh <jksingh112021@gmail.com>
2022-06-30 18:40:37 +0530

    adding edit/forget password functionality

--- rails/scheduled_tweets/config/routes.rb
+++ rails/scheduled_tweets/config/routes.rb
@@ -2,11 +2,17 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "about", to: "about#index"
 
+  get "password", to: "passwords#edit", as: :edit_password
+  patch "password", to: "passwords#update"
+
   get "sign_up", to: "registrations#new"
   post "sign_up", to: "registrations#create"
 
   get "sign_in", to: "sessions#new"
   post "sign_in", to: "sessions#create"
+
+  get "password/reset", to: "password_resets#new"
+  get "password/reset", to: "password_resets#create"
 
   delete "logout", to: "sessions#destroy"
   # Defines the root path route ("/")
