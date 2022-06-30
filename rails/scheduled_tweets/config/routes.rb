6550bdddbc6b5ff723d245673ad24a439a8ff927
Jaskaran Singh <jksingh112021@gmail.com>
2022-06-30 20:04:22 +0530

    Action mailer and reset password token mailer

--- rails/scheduled_tweets/config/routes.rb
+++ rails/scheduled_tweets/config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   post "sign_in", to: "sessions#create"
 
   get "password/reset", to: "password_resets#new"
-  get "password/reset", to: "password_resets#create"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 
   delete "logout", to: "sessions#destroy"
   # Defines the root path route ("/")
