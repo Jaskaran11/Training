f78b13d94cf8d919f5343275d1d3af2848a16a0b
Jaskaran Singh <jksingh112021@gmail.com>
2022-07-27 20:24:19 +0530

    using gem devise for users

--- assignment/lms/config/routes.rb
+++ assignment/lms/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'authors#index'
+  #root 'authors#index'
   resources :authors
-
+  root 'home#homepage'
+  get 'homepage', to: 'home#homepage'
+  get 'dashboard', to: 'home#dashboard'
 end
