c814755589adb0f4a295707a8dd3582e1e6cbaea
Jaskaran Singh <jksingh112021@gmail.com>
2022-07-27 19:01:06 +0530

    creating new,show,edit,update,destroy actions

--- assignment/lms/app/controllers/authors_controller.rb
+++ assignment/lms/app/controllers/authors_controller.rb
@@ -2,4 +2,35 @@ class AuthorsController < ApplicationController
   def index
     @author = Author.all
   end
+
+  def show
+    @author = Author.find(params[:id])
+  end
+
+  def new 
+    @author = Author.new
+  end
+
+  def edit 
+    @author = Author.find(params[:id])
+  end
+
+  def update 
+    @author = Author.find(params[:id])
+    if @author.update(author_params)
+        redirect_to authors_path(@author)
+    else 
+      render :edit
+  end
+end
+
+  def destroy
+    @author.destroy 
+    redirect_to authors_index_path
+  end
+
+  private 
+  def authors_params
+    params.require(:author).permit(:name, :email, :phone_no, :dob, :hobby, :skill)
+  end
 end
