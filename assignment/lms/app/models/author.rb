0690fb3915ff1002cfd54b39012521b506b2a461
Jaskaran Singh <jksingh112021@gmail.com>
2022-07-29 15:58:10 +0530

    adding validations to author model

--- assignment/lms/app/models/author.rb
+++ assignment/lms/app/models/author.rb
@@ -1,7 +1,6 @@
 class Author < ApplicationRecord
   has_many :books
-
-      
-  
-   
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :phone_no, numericality: true
 end
