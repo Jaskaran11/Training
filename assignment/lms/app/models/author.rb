9e334b3c8ce8088b00fd6719408095499ec8d2aa
Jaskaran Singh <jksingh112021@gmail.com>
2022-08-01 17:57:51 +0530

    using validates_associated and validates_each validation in author model

--- assignment/lms/app/models/author.rb
+++ assignment/lms/app/models/author.rb
@@ -1,6 +1,10 @@
 class Author < ApplicationRecord
   has_many :books, dependent: :destroy
-  #validates :name, presence: true
-  #validates :email, uniqueness: true
-  #validates :phone_no, numericality: true
+  validates :name, presence: true
+  validates :email, uniqueness: true
+  validates :phone_no, numericality: true
+  validates_associated :books
+  validates_each :name, :surnmae do |record, attr, value|
+    record.errors.add(attr, 'must start with upper case') if value =~ /\A[[:lower]]/
+  end
 end
