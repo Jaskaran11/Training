d7eb14758569cd1fbb08d00c2e50286bf8452807
Jaskaran Singh <jksingh112021@gmail.com>
2022-08-09 19:01:43 +0530

    use factory bot method create and build_stubbed

--- assignment/lms/spec/factories/admins.rb
+++ assignment/lms/spec/factories/admins.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :admin do
     name { "MyString" }
-    age { 1 }
+    age { rand(1..100) }
     born_at { "2022-08-09 18:02:20" }
+    sequence(:email) {|n| "name+#{n}@example.com" }
   end
 end
--- assignment/lms/spec/models/admin_spec.rb
+++ assignment/lms/spec/models/admin_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "must have an age greater than 21" do
+    admin = FactoryBot.create(:admin)
+    expect(admin.age).to be > 21
+  end
+  it "is valid if email present" do
+    admin = FactoryBot.build_stubbed(:admin)
+    expect(admin.email).to eq("name+2example.com")
+  end
 end
