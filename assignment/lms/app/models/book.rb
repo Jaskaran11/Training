e84caf903dfdf4c5b4ed6d7c33165ca37de2c267
Jaskaran Singh <jksingh112021@gmail.com>
2022-08-11 16:30:34 +0530

    testing model method in rspec

--- assignment/lms/app/models/book.rb
+++ assignment/lms/app/models/book.rb
@@ -1,9 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :author
-  
+
   after_destroy :log_destroy_action
 
-  def log_destroy_action 
+  def log_destroy_action
     puts 'Book destroyed'
+  end
+  validates :title, presence: true
+
+  def formatted_name
+    "#{publisher} #{title}"
   end
 end
--- assignment/lms/spec/models/book_spec.rb
+++ assignment/lms/spec/models/book_spec.rb
@@ -1,12 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Book, type: :model do
+RSpec.describe Book do
   describe 'validations' do
     describe 'title' do
       it 'must be present' do
         book = Book.new
         expect(book).to_not be_valid
       end
+    end
+  end
+
+  describe '#formatted_name' do
+    it 'returns the publisher and title in a string' do
+      expect(Book.formatted_name).to eql('Casemate Publishers Look Homeward, Angel')
     end
   end
 end
