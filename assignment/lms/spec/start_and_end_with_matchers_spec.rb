fd003bf76d252620ef379991ddd3912e1e2193af
Jaskaran Singh <jksingh112021@gmail.com>
2022-08-12 18:36:05 +0530

    start_with and end_with matcher

---
+++ assignment/lms/spec/start_and_end_with_matchers_spec.rb
@@ -0,0 +1,19 @@
+RSpec.describe 'start_with and end_with matchers' do
+  describe 'itsachekmate' do
+    it 'should check for substring at the beginning or end' do
+      expect(subject).to start_with('its')
+      expect(subject).to end_with('mate')
+    end
+
+    it { is_expected.to start_with('its') }
+    it { is_expected.to end_with('mate') }
+  end
+
+  describe [:a, :b, :c, :d] do
+    it 'should check for elements at beginning or end of the array' do
+      expect(subject).to start_with(:a)
+      expect(subject).to start_with(:a)
+      expect(subject).to end_with(:c, :d)
+    end
+  end
+end
