9a21e6c51f2e55ecb400d34c0b5e343b29f6a0e1
Jaskaran Singh <jksingh112021@gmail.com>
2022-08-12 14:10:02 +0530

    action mailer test using rsec

--- assignment/lms/spec/mailers/crud_notfication_spec.rb
+++ assignment/lms/spec/mailers/crud_notfication_spec.rb
@@ -1,8 +1,19 @@
 require "rails_helper"
 
-RSpec.describe "CrudNotficationMailer", type: :mailer do
-  test "create_notification" do
-      expect(mail.subject).to eq("Create")
+RSpec.describe CrudNotificationMailer, type: :mailer do
+  describe "mail sent" do
+    let(:author) { FactoryBot.create(:author) }
+
+    let(:mail) { CrudNotificationMailer.create_notification(author) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("A new entry for Author has been created.")
       expect(mail.to).to eq(["admin@example.com"])
+      expect(mail.from).to eq(["from@example.com"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Hello")
     end
   end
+end
--- assignment/lms/spec/mailers/previews/crud_notfication_preview.rb
+++ assignment/lms/spec/mailers/previews/crud_notfication_preview.rb
@@ -3,8 +3,7 @@ class CrudNotficationPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/crud_notfication/new
   def create_notification
-    author = Author.new(email: "test@test.com")
+    author = FactoryBot.create(:author)
     CrudNotificationMailer.create_notification(author)
   end
-
 end
